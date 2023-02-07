23ae40b4d5a69c7c3a082a1f4fc78f0a9f888f41
Alex <alex@Freezeman>
2023-02-08 02:16:03 +0800

    added str_func_cap и докстринги

---
+++ str_func.py
@@ -0,0 +1,14 @@
+def str_func():
+    """
+    все буквы заглавные
+    """
+    word = input("Введите слово: ")
+    return word.upper()
+
+
+def str_func_cap():
+    """
+    первые буквы заглавные
+    """
+    words = input("Введите слово: ")
+    return words.capitalize()
